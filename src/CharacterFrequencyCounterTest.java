1df542e4ac8e9d4aad8d715ebce78fd21c168fb1
rjfredr <fredrickalexander1727@gmail.com>
2025-01-20 17:07:48 -0800

    01.20.2025 5:07pm commit, i literally followed the panopto recording to a T and i still get error even though i restarted the repo like twice???

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -1,7 +1,16 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+    @Test
+    public void testLAppearsThriceInHelloWorld() {
+        //Arrange set up test
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("Hello world");
+        //Act get frequency
+        int actualCount = counter.getFrequency('l');
+        //Assert check if equals
+        assertEquals(3, actualCount);
+    }
 }
